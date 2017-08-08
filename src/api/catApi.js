ea8a1778500fa59055e65890fef42d26233a5105
Tales Tonini <ttonini@odecee.com.au>
2017-08-08 17:05:55 +1000

    cleaning repo

--- src/api/catApi.js
+++
@@ -1,9 +0,0 @@
-class CatApi {
-  static getAllCats() {
-    return fetch('http://localhost:5000/api/v1/cats')
-      .then(response => response.json())
-      .catch(error => error)
-  }
-}
-
-export default CatApi
--- src/api/hobbyApi.js
+++
@@ -1,9 +0,0 @@
-class HobbyApi {
-  static getAllHobbies() {
-    return fetch('http://localhost:5000/api/v1/hobbies')
-      .then(response => response.json())
-      .catch(error => error)
-  }
-}
-
-export default HobbyApi
